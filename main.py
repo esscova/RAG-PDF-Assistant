1f8f4f168ff187fdc76091a2aec7e49dca3425c0
esscova <wsantos08@hotmail.com>
2026-02-09 09:55:03 -0300

    feat(main): update main para inicializar RAGChatBot

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from pdf-rag-chatbot!")
+from src.chatbot import RAGChatBot
 
+bot = RAGChatBot()
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    print('-'*100)
+    print('\n= MODO CHAT =')
+    print('-'*100)
+    bot.chat('Qual a capital da frança?', use_rag=False)
+
+    print('-'*100)
+    print('\n= MODO RAG =')
+    print('-'*100)
+    bot.load_documents("C:\\Users\\wsant\\Downloads\\O'Reilly - PT - SQL Guia Prático - Alice Zhao.pdf")
+    bot.chat('Como usar o comando "select"?')
